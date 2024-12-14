c6c29e679f8b35fc01935e701264742260e6246e
School-911 <nguyendangtruong2004thd@gmail.com>
2024-12-14 20:35:44 +0700

    Update Capnhatnv.php

--- Capnhatnv.php
+++ Capnhatnv.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý nhân viên</title>
-    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="Styles.css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> 
 </head>
